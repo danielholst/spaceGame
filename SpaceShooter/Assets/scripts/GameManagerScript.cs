9a215d524a392fd8248ada8dcac8b35ec800a484
Daniel <danielholst2@gmail.com>
2018-05-20 23:02:21 +0200

    add gui timer

---
+++ SpaceShooter/Assets/scripts/GameManagerScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* Game manager (single-player)
+ * Here the time and score of the game is handled.
+ * For a multiplayer game this is handled in the networkManager? 
+ */
+public class GameManagerScript : MonoBehaviour {
+
+    private int numberOfPlayers;
+    private float timer;
+
+    // Use this for initialization
+    void Start () {
+        timer = 120.0f; // 2minutes
+    }
+ 
+    // Update is called once per frame
+    void Update () {
+
+        timer -= Time.deltaTime;
+
+        // end of round
+        if (timer < 0)
+        {
+            timer = 0;
+            Debug.Log("End of round");
+        }
+    }
+
+    public float getTime()
+    {
+        return timer;
+    }
+}
---
+++ SpaceShooter/Assets/scripts/GuiTimerScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GuiTimerScript : MonoBehaviour
+{
+    GameObject gameManager;
+    private int time;
+    private Text text;
+
+    // Use this for initialization
+    void Start()
+    {
+        gameManager = GameObject.FindGameObjectWithTag("GameManager");
+        text = GetComponent<Text>();
+        time = 0;
+    }
+
+    // Update is called once per frame
+    void OnGUI()
+    {
+        time = Mathf.RoundToInt(gameManager.GetComponent<GameManagerScript>().getTime());
+        text.text = time.ToString();
+    }
+}
