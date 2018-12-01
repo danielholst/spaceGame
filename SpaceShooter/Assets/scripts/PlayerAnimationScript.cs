c1addfafa22d0df8c0f0bf5de5b7358d59956436
Daniel <danielholst2@gmail.com>
2018-12-01 22:38:28 +0100

    add animation of alien when shooting

---
+++ SpaceShooter/Assets/scripts/PlayerAnimationScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+/*
+ * Script that handles animations of the Alien.
+ */
+public class PlayerAnimationScript : MonoBehaviour
+{
+    private Animator anim;
+    int jiggleHash;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        jiggleHash = Animator.StringToHash("jiggle");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))// && (stateInfo.fullPathHash != jiggleHash))
+        {
+            anim.SetTrigger(jiggleHash);
+        }
+    }
+}
